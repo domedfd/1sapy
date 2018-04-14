c201161a9033a70077c4fc515346632420cc73c9
Domenico Delvalle <domedfd@gmail.com>
2018-04-14 15:42:22 -0400

    Modificaciones en las rutas y modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,38 +6,25 @@ import { HttpModule } from '@angular/http';
 //Rutas
 import { APP_ROUTES } from './app.routes';
 
+//Modulos
+import { PagesModule } from './pages/pages.module';
+import { LoginModule } from './login/login.module';
+
+//Componete
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { PagesComponent } from './pages/pages.component';
-import { RegisterComponent } from './login/register.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent,
-    PagesComponent,
-    RegisterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PagesModule,
+    LoginModule
   ],
   providers: [],
   bootstrap: [AppComponent]
